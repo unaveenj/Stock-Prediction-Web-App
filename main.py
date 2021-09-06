4e6e9e6f1e160ca68e9d16dfd4b3fcdf04fe717c
Naveen <56919721+unaveenj@users.noreply.github.com>
2021-09-07 00:01:22 +0800

    indian stock support done

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ TODAY_DATE = date.today().strftime('%Y-%m-%d')
 #Title of webapp
 st.title("Stock predictor")
 st.sidebar.info("Choose US stocks or ETFs")
-selection=['US','ETF','Crypto']
+selection=['US','ETF','IND','Crypto']
 main_selection=st.sidebar.selectbox('Select US stock or ETF',selection)
 st.sidebar.text("Credits to : @python-engineer tutorial")
 
@@ -136,6 +136,75 @@ if main_selection=='US':
             st.warning("Stock doesn't exist in database")
         else:
             st.warning(f"Did u mean : {error}")
+
+
+
+elif main_selection=='IND':
+    df = pd.read_csv("indian.csv")
+    st.header("Indian Stocks!")
+    st.text("All prices are in INR")
+    stocks = tuple(df['Symbol'])
+    stock_name = tuple(df['Name'])
+    # print(stocks)
+    # select_stocks = st.selectbox("Select stock for prediction",stocks)
+    local_css("style.css")
+    remote_css('https://fonts.googleapis.com/icon?family=Material+Icons')
+    st.text("Search for stock below")
+    select_stocks = st.text_input("", "").upper()
+    button_clicked = st.button("OK")
+    if select_stocks in stocks:
+        selected_name = stock_name[stocks.index(select_stocks)]
+        st.text(f"{selected_name}")
+        period=st.slider("Number of days:", 1,4*365)
+
+
+
+        data_load_state = st.text("Load data...")
+        data = load_stock_data(select_stocks)
+        data_load_state.text("Loading data done !!!")
+        check = st.checkbox('Click here for candlestick chart')
+        if check:
+            plot_candlestick(data)
+        # data['Date'] = data['Date'].dt.strftime('%d-%m-%Y')
+        st.subheader("Statistics")
+        st.write(data)
+
+
+
+        plot_graph()
+
+        # Forecasting
+
+        df_train = data[['Date','Close']]
+        df_train = df_train.rename(columns={"Date":"ds","Close":"y"})
+
+        model=Prophet()
+        model.fit(df_train)
+        predictions = model.make_future_dataframe(periods=period)
+        forecast = model.predict(predictions)
+        # forecast['ds'] = forecast['ds'].dt.strftime('%d-%m-%Y')
+        st.subheader("Forecast Data")
+        st.write(forecast)
+
+        fig1=plot_plotly(model,forecast)
+        st.plotly_chart(fig1)
+
+        st.write("Forecast components")
+        fig2=model.plot_components(forecast)
+        st.write(fig2)
+
+    elif select_stocks=="":
+        st.warning("Please search for your stock")
+    elif select_stocks not in stocks:
+        error = difflib.get_close_matches(select_stocks, stocks)
+
+        if len(error)==0 :
+            st.warning("Stock doesn't exist in database")
+        else:
+            st.warning(f"Did u mean : {error}")
+
+
+
 
 
 elif main_selection=='Crypto':
